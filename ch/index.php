7ce5d3b699eaf311f6d4ce5f117f6a3f3b9dfc4f
Killua Fourteen <zenn130203@gmail.com>
2025-03-23 10:10:47 +0700

    Update index.php

--- ch/index.php
+++ ch/index.php
@@ -1,16 +1,14 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-  <head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8">
-  <meta name="generator" content="PSPad editor, www.pspad.com">
-  <?php
-/* This will give on error. Note the output
-* above, which is before the header() call */
-header('Location: https://whatsapp.com/channel/0029VaWXWTD8kyyOvgdf1o3x');
-exit;
-?>
-  </head>
-  <body>
-
-  </body>
-<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta name="generator" content="PSPad editor, www.pspad.com">
+    <?php
+    // Mengarahkan pengguna ke link WhatsApp
+    header('Location: https://whatsapp.com/channel/0029VaWXWTD8kyyOvgdf1o3x');
+    exit;
+    ?>
+</head>
+<body>
+</body>
+</html>
