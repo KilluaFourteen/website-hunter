893042571525c2928a2abd7124ec475e8033b3df
Killua Fourteen <zenn130203@gmail.com>
2025-03-23 09:39:52 +0700

    Create index.php

---
+++ ch/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta name="generator" content="PSPad editor, www.pspad.com">
+  <?php
+/* This will give on error. Note the output
+* above, which is before the header() call */
+header('Location: https://whatsapp.com/channel/0029VaWXWTD8kyyOvgdf1o3x');
+exit;
+?>
+  </head>
+  <body>
+
+  </body>
+<html>
